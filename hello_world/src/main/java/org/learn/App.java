a5abf09e2cbc51ae9150022509ab9dd4782db369
long <wuyunlonggo@163.com>
2022-06-21 00:54:08 +0800

    [+] 添加JDBC示例性代码

--- hello_world/src/main/java/org/learn/App.java
+++ hello_world/src/main/java/org/learn/App.java
@@ -4,6 +4,9 @@ package org.learn;
  * Hello world!
  */
 public class App {
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
