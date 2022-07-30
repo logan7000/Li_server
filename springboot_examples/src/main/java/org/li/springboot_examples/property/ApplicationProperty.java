1323c4d8133156746d290b2d6b5711595bb8ccb9
loganylwu <loganylwu@tencent.com>
2022-07-30 14:16:36 +0800

    [+] add module

---
+++ springboot_examples/src/main/java/org/li/springboot_examples/property/ApplicationProperty.java
@@ -0,0 +1,19 @@
+package org.li.springboot_examples.property;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * 两种不同的读取变量的方式
+ * 这种没有前缀，直接全路径获取
+ */
+@Data
+@Component
+public class ApplicationProperty {
+    @Value("${application.name}")
+    private String name;
+    @Value("${application.version}")
+    private String version;
+
+}
---
+++ springboot_examples/src/main/java/org/li/springboot_examples/property/DeveloperProperty.java
@@ -0,0 +1,18 @@
+package org.li.springboot_examples.property;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+/**
+ * 两种不同的读取变量的方式
+ * 这种友前缀，可以根据前缀获取内部的所有属性
+ */
+@Data
+@ConfigurationProperties(prefix = "developer")
+@Component
+public class DeveloperProperty {
+    private String name;
+    private String website;
+    private String qq;
+    private String phoneNumber;
+}
