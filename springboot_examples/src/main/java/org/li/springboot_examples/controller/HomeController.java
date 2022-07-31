f6127658a5fd87c191e4dc398861794988610a37
loganylwu <loganylwu@tencent.com>
2022-07-31 11:21:07 +0800

    [+] 添加静态资源按钮控制

--- springboot_examples/src/main/java/org/li/springboot_examples/controller/HomeController.java
+++ springboot_examples/src/main/java/org/li/springboot_examples/controller/HomeController.java
@@ -1,13 +1,19 @@
 package org.li.springboot_examples.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.HashMap;
 
-@RestController
+@Controller
 public class HomeController {
+    @RequestMapping("")
+    public String index(){
+        return "index";
+    }
     @RequestMapping("hello")
+    @ResponseBody
     public HashMap<String, String> hello() {
         HashMap<String, String> hashMap = new HashMap<>();
         hashMap.put("msg", "hello world");
